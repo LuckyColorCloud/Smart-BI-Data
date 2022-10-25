014caf9671b96e17986939a053898ed1c9ea926d
Sober <sober1050@qq.com>
2022-10-25 15:19:17 +0800

    初步搭建

---
+++ bi-security/bi-security-service/src/main/java/com/yun/bisecurity/BiSecurityServiceApplication.java
@@ -0,0 +1,26 @@
+package com.yun.bisecurity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Yun
+ */
+@Slf4j
+@SpringBootApplication
+@MapperScan(value = "com.yun.bisecurity.dao")
+@ComponentScan(basePackages = {"com.yun.bisecurity","com.yun.bidataconnmon"})
+@EnableFeignClients(basePackages = {
+        "com.yun.*.api"
+})
+public class BiSecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiSecurityServiceApplication.class, args);
+    }
+
+}
